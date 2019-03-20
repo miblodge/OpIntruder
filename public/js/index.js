f84c9576527ac2fbb83147510022c4222985c6f1
DanaVarahi <dana_varahi@protonmail.com>
2019-03-20 17:56:43 +0000

    creating array of selected imgs for the slideshow

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,56 @@
+var bttnName;
+var allImgs;
+var selectedImgs;
 
 $(document).ready(function(){
 
+allImgs = $('.container div').siblings();
+
+//Hides the photo grid
   $(".container div").hide();
 
+//Shows sellection of photos with class matching menu button id
   var button = $("button");
 
   $('button').on('click',function(){
-    var bttnName = this.id;
+    bttnName = this.id;
 
     console.log(bttnName);
+
         $("#homeImg").hide();
         $(".container div").hide();
         $("div." +bttnName).show();
   });
 
+//Hides the photo grid and shows home img istead
   $("#home").on("click", function(){
     $("#homeImg").show();
   });
 
-$("img").on("click",function(){
-  var newDiv=$('<div class = slideshow></div>');
-  var divClass = $(this).parent().attr('class');
+//Opens up a slideshow viewer
+  $("img").on("click",function(){
+    var slideshowDiv=$('<div class = slideshow></div>');
+    var currentImg = $(this);
 
+    if ( $('.slideshow').length > 0){
+      $('.slideshow').empty();
+      $(this).clone().prependTo('.slideshow');
+    } else {
+      $('body').append(slideshowDiv);
+      $(this).clone().prependTo(slideshowDiv);
+    }
 
- if ( $('.slideshow').length > 0){
-   $('.slideshow').empty();
-   $(this).clone().prependTo('.slideshow');
- } else {
-   $('body').append(newDiv);
-   $(this).clone().prependTo(newDiv);
- }
+//creates a selection of slides maching menu id.
+selectedImgs=[];
 
+$(allImgs).each(function(i){
+  console.log(i+': '+$(this).attr('class'));
+  if($(this).hasClass(bttnName)) {
+    console.log('has class '+bttnName);
+    selectedImgs.push($(this).clone());
+  }
+  });
 
-  console.log(divClass);
-});
+   console.log(selectedImgs);
+    });
 });
