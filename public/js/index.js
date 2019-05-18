5c22f870687e00b79ae54ce0acd9e022b7f95979
DanaVarahi <dana_varahi@protonmail.com>
2019-05-18 16:32:09 +0100

    Slideshow working cycling through all images and returning to the first in an infinite loop.
    some refactoring.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,24 @@
 let bttnName;
 let allImgs;
 let selectedImgs;
+let currentImg;
+
+// finds index number of currentImg in selectedImgs array
+function matchSrc(src){
+     return src == currentImg.prop('src');
+   }
+
+// cycels through the images inside slideshow slideshowDiv
+function nextImg(index, slideshow){
+  $(slideshow).eq(index).hide();
+
+  if (++index >= slideshow.length){
+   index = 0;
+  }
+  $(slideshow).eq(index).show();
+  console.log(index+" "+slideshow.length);
+  setTimeout(function(){ nextImg(index,slideshow) }, 3000);
+}
 
 $(document).ready(function(){
 
@@ -22,6 +40,7 @@ $(document).ready(function(){
         $("div." +bttnName).show();
   });
 
+
 //Hides the photo grid and shows home img istead
   $("#home").on("click", function(){
     $("#homeImg").show();
@@ -29,12 +48,12 @@ $(document).ready(function(){
 
 //Opens up a slideshow viewer
   $("img").on("click",function(){
-    let slideshowDiv=$('<div class = slideshow></div>');
-    let currentImg = $(this);
-    let selectedImgs=[];
+    let slideshowDiv=$('<div id = slideshow></div>');
+    currentImg = $(this);
+    selectedImgs=[];
 
-    if ( $('.slideshow').length > 0){
-      $('.slideshow').empty();
+    if ( $('#slideshow').length > 0){
+      $('#slideshow').empty();
     } else {
       $('body').append(slideshowDiv);
     }
@@ -48,28 +67,22 @@ $(document).ready(function(){
 
 // inserts selection of slides into slideshow div
   $(selectedImgs).each(function() {
-    $(".slideshow").append("<img src='"+this+"'>");
+    $("#slideshow").append("<img src='"+this+"'>");
   });
 
 // and hides them
   $(slideshowDiv).children().hide();
 
-// finds index number of currentImg in selectedImgs array
-function matchSrc(src){
-     return src == currentImg.prop('src');
-   }
-
 let i = selectedImgs.findIndex(matchSrc);
 console.log(i);
 
 //finds element with maching index inside slideshowDiv and shows it
-$(slideshowDiv).children().eq(i).show();
+let currentSlideshow= $(slideshowDiv).children();
+let visibleImg =$(currentSlideshow).eq(i);
+$(visibleImg).show();
 
-let prev = selectedImgs[i-1];
-let next = selectedImgs[i+1];
+setTimeout(function(){ nextImg(i,currentSlideshow) }, 3000);
 
-//console.log(prev);
-//console.log(next);/*on the last img it returns undefined*/
 
  /*To do:
       - cycle through the array of selectedImgs (function to come up with, set interval)
